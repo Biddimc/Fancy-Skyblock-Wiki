b9fbd1a4f9848166e3e5e8696a369ad0f9ae63d5
Biddimc <biddimc@gmail.com>
2024-12-22 15:09:35 +0100

    Update texture.js

--- texture.js
+++ texture.js
@@ -6,15 +6,21 @@ document.getElementById("TextureChanger").addEventListener("click",
     function() {
         if (custom === "no"){
             //ändere Bild
-            document.getElementById("f_t_pic").
+            document.getElementById("farming_talisman_pic").
             src = "https://raw.githubusercontent.com/Biddimc/Fancy-Skyblock-Wiki/refs/heads/main/textures/Artifakte/Ge%C3%A4ndert/farming_talisman.png";
+
+            document.getElementById("farmer_orb_pic").
+            src = "https://raw.githubusercontent.com/Biddimc/Fancy-Skyblock-Wiki/refs/heads/main/textures/Artifakte/Ge%C3%A4ndert/farmer_orb.png"
 
 
             custom ="yes";
         }
         else{
-            document.getElementById("f_t_pic").
+            document.getElementById("farming_talisman_pic").
             src ="https://raw.githubusercontent.com/Biddimc/Fancy-Skyblock-Wiki/refs/heads/main/textures/Artifakte/Standard/SkyBlock_items_farming_talisman.png"
+
+            document.getElementById("farmer_orb_pic").
+            src = "https://raw.githubusercontent.com/Biddimc/Fancy-Skyblock-Wiki/refs/heads/main/textures/Artifakte/Standard/SkyBlock_items_farmer_orb.png"
 
             custom = "no"
         }
